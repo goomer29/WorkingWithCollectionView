db76928b9ae16d58a5f8849444a262a98c0778ae
omergo hi <omergolan92@gmail.com>
2023-03-11 23:29:57 +0200

    collectiob view

--- WorkingWithCollectionView/MainPage.xaml.cs
+++ WorkingWithCollectionView/MainPage.xaml.cs
@@ -15,8 +15,9 @@ public partial class MainPage : ContentPage
 
     #region בכל שינוי בשדות נפעיל את האירוע
     public Student Student { get => _student; set { if (_student != value) { _student = value; OnPropertyChanged("Student"); } } }
-    
+
     #endregion
+    public Student current;
 
     public MainPage()
     {
@@ -24,6 +25,7 @@ public partial class MainPage : ContentPage
         Students = new ObservableCollection<Student>();
         //נאתחל את התלמיד הבודד לריק
         Student = new() { Image="dotnet_bot.svg", Name="ברירת מחדל",BirthDate=new DateTime()};
+        current = null;
        
         //נקשר את הדף שלנו לאובייקט המכיל את הקוד שלו
         this.BindingContext = this;
@@ -45,8 +47,17 @@ public partial class MainPage : ContentPage
         Students.Add(new() { Name = "Roni", Image = "roni.jpg", BirthDate = new DateTime(2006, 2, 19) });
         //הוספת תלמיד בדרך המלאה
         Students.Add(new Student { Name = "Omer", BirthDate = new DateTime(2006, 2, 9), Image = "omer.jpg" });
+        //כל התאריכים ששמתי נכונים בטווח של פלוס מינוס 2 שנים
+        Students.Add(new Student { Name = "Avital", BirthDate = new DateTime(2006, 7, 13), Image = "avital.jpg" });
+        Students.Add(new Student { Name = "Avigdor", BirthDate = new DateTime(2006, 4, 18), Image = "aviv.jpg" });
+        Students.Add(new Student { Name = "Gil", BirthDate = new DateTime(2006, 9, 22), Image = "gil.jpg" });
+        Students.Add(new Student { Name = "Itamar", BirthDate = new DateTime(2006, 8, 19), Image = "itamar.jpg" });
+        Students.Add(new Student { Name = "Jofir", BirthDate = new DateTime(2006, 4, 20), Image = "jofir.jpg" });
+        Students.Add(new Student { Name = "Maya", BirthDate = new DateTime(2006, 10, 25), Image = "maya.jpg" });
+        Students.Add(new Student { Name = "Ofek", BirthDate = new DateTime(2006, 6, 28), Image = "ofek.jpg" });
+        Students.Add(new Student { Name = "OfekMan", BirthDate = new DateTime(2006, 3, 14), Image = "ofekman.jpg" });
+        Students.Add(new Student { Name = "Yali", BirthDate = new DateTime(2006, 9, 12), Image = "yali.jpg" });
         Student = Students[0];
-
     }
 
     private void Button_Clicked(object sender, EventArgs e)
@@ -54,6 +65,26 @@ public partial class MainPage : ContentPage
         LoadStudents();
         OnPropertyChanged(nameof(Students));
 
+    }
+    private void ButtonDel_Clicked(object sender, EventArgs e)
+    {
+        this.Students.Clear();
+        OnPropertyChanged(nameof(Students));
+
+    }
+    private void SwipeL_Clicked(object sender, EventArgs e)
+    {
+        Students.Remove(current);
+        OnPropertyChanged(nameof(Students));
+    }
+    private void SwipeR_Clicked(object sender, EventArgs e)
+    {
+        Students.Add(current);
+        OnPropertyChanged(nameof(Students));
+    }
+    private void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        current = e.CurrentSelection as Student;
     }
 }
 
